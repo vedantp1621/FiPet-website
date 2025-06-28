54dab5c9157870ea19e49ef39bdb2030126a6c9c
Jitesh-Gadage16 <gadagejitesh16@gmail.com>
2025-06-28 18:47:12 -0400

    Done with creating footer for mobile reponsive

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import './globals.css'
+import React from 'react'
+import MobileFooter from '@/components/MobileFooter';
+
+
+
+const page = () => {
+    return (
+
+        <MobileFooter />
+
+
+
+    )
+}
+
+export default page
