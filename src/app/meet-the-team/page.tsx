5fe526d5f79bbd92a512a267ceeb057b63f20099
ssklin <shengkuo.lin@outlook.com>
2025-06-29 20:25:23 -0700

    fix(meet-the-team): apply gradient color to "And we have fun doing it ;)" text and add outer app directory

--- src/app/meet-the-team/page.tsx
+++ src/app/meet-the-team/page.tsx
@@ -8,7 +8,7 @@ export default function TeamPage() {
     <div className={`${styles.customBg} ${styles.grayText} py-8`}>
       <div className="w-full max-w-6xl mx-auto px-4">
         <h1 className="text-4xl font-bold">We&apos;re shaping the way teens learn finance.</h1>
-        <h6 className={`text-3xl font-bold ${styles.orangeText}`}>And we have fun doing it ;)</h6>
+        <h6 className={`text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#F97316] to-[#F99E16]`}>And we have fun doing it ;)</h6>
         <p className="text-2xl font-semibold my-5">Meet the team</p>
         <div className="grid grid-cols-2 sm:grid-cols-4 gap-x-12 gap-y-6 mt-6 mb-12">
           {teamHeads.map((teamHead, idx) => (
